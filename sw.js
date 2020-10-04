d0e874357ff570e2f5b218727f3761a67b635d04
aghiljv <aghiljv@gmail.com>
2020-10-04 13:56:22 +0530

    pwa tests

--- sw.js
+++ sw.js
@@ -28,5 +28,6 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('https://aghiljose.com/.*'), new workbox.strategies.CacheFirst ({"cacheName":"aghil_jose","cacheExpiration":{"maxEntries":100,"maxAgeSeconds":864000}}), 'GET')
 workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.StaleWhileRevalidate ({}), 'GET')
