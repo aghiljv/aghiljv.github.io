8d1b037722351f7e29142f86effea285d249e7ab
aghiljv <aghiljv@gmail.com>
2021-03-02 13:06:24 +0530

    minor corrections

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ workbox.precaching.cleanupOutdatedCaches();
 workbox.routing.registerRoute(
   new RegExp("https://aghiljose.com/.*"),
   new workbox.strategies.CacheFirst({
-    cacheName: "aghil_jose_260221_01",
+    cacheName: "aghil_jose_020321_01",
     cacheExpiration: { maxEntries: 100, maxAgeSeconds: 604800 }
   }),
   "GET"
