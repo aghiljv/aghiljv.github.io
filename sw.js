f733e245ea45c5276074322fae1477967e9bcaa3
aghiljv <aghiljv@gmail.com>
2021-03-21 11:46:19 +0530

    new article -  SF

--- sw.js
+++ sw.js
@@ -33,7 +33,7 @@ workbox.precaching.cleanupOutdatedCaches();
 workbox.routing.registerRoute(
   new RegExp("https://aghiljose.com/.*"),
   new workbox.strategies.CacheFirst({
-    cacheName: "aghil_jose_120321_01",
+    cacheName: "aghil_jose_210321_01",
     cacheExpiration: { maxEntries: 100, maxAgeSeconds: 604800 }
   }),
   "GET"
