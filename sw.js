5a2645fd86b4c6a7882e07a1dc3d6e23bc296e08
aghiljv <aghiljv@gmail.com>
2021-06-01 12:15:17 +0530

    new portContent

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js')
+importScripts(
+  "https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js"
+);
 
 // --------------------------------------------------
 // Configure
@@ -6,16 +8,16 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw
 
 // Set workbox config
 workbox.setConfig({
-  "debug": false
-})
+  debug: false
+});
 
 // Start controlling any existing clients as soon as it activates
-workbox.core.clientsClaim()
+workbox.core.clientsClaim();
 
 // Skip over the SW waiting lifecycle stage
-workbox.core.skipWaiting()
+workbox.core.skipWaiting();
 
-workbox.precaching.cleanupOutdatedCaches()
+workbox.precaching.cleanupOutdatedCaches();
 
 // --------------------------------------------------
 // Precaches
@@ -28,9 +30,24 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('https://aghiljose.com/.*'), new workbox.strategies.CacheFirst ({"cacheName":"aghil_jose_270321_01","cacheExpiration":{"maxEntries":100,"maxAgeSeconds":604800}}), 'GET')
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.StaleWhileRevalidate ({}), 'GET')
+workbox.routing.registerRoute(
+  new RegExp("https://aghiljose.com/.*"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "aghil_jose_010621",
+    cacheExpiration: { maxEntries: 100, maxAgeSeconds: 604800 }
+  }),
+  "GET"
+);
+workbox.routing.registerRoute(
+  new RegExp("/_nuxt/"),
+  new workbox.strategies.CacheFirst({}),
+  "GET"
+);
+workbox.routing.registerRoute(
+  new RegExp("/"),
+  new workbox.strategies.StaleWhileRevalidate({}),
+  "GET"
+);
 workbox.routing.setCatchHandler(({ url, event, params }) => {
   const strategy = new workbox.strategies.NetworkFirst({
     networkTimeoutSeconds: 10
