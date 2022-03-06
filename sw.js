53c0cb8fd5f8d67fc31821d9b17e3481156e3e14
aghiljv <aghiljv@gmail.com>
2022-03-06 14:16:40 +0530

    minor updates due to error in component registrations

--- sw.js
+++ sw.js
@@ -1,41 +1,114 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js')
+const options = {"workboxURL":"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.js","importScripts":[],"config":{"debug":false},"cacheOptions":{"cacheId":"website-prod","directoryIndex":"/","revision":"Fuv84rJgV4Dm"},"clientsClaim":true,"skipWaiting":true,"cleanupOutdatedCaches":true,"offlineAnalytics":false,"preCaching":[{"revision":"Fuv84rJgV4Dm","url":"https://aghiljose.com"}],"runtimeCaching":[{"urlPattern":"https://aghiljose.com/.*","handler":"CacheFirst","method":"GET","strategyOptions":{"cacheName":"aghil_jose_060322","cacheExpiration":{"maxEntries":100,"maxAgeSeconds":604800}},"strategyPlugins":[]},{"urlPattern":"/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/","handler":"StaleWhileRevalidate","method":"GET","strategyPlugins":[]}],"offlinePage":null,"pagesURLPattern":"/","offlineStrategy":"StaleWhileRevalidate"}
 
-// --------------------------------------------------
-// Configure
-// --------------------------------------------------
+importScripts(...[options.workboxURL, ...options.importScripts])
 
-// Set workbox config
-workbox.setConfig({
-  "debug": false
-})
+initWorkbox(workbox, options)
+workboxExtensions(workbox, options)
+precacheAssets(workbox, options)
+cachingExtensions(workbox, options)
+runtimeCaching(workbox, options)
+offlinePage(workbox, options)
+routingExtensions(workbox, options)
 
-// Start controlling any existing clients as soon as it activates
-workbox.core.clientsClaim()
+function getProp(obj, prop) {
+  return prop.split('.').reduce((p, c) => p[c], obj)
+}
 
-// Skip over the SW waiting lifecycle stage
-workbox.core.skipWaiting()
+function initWorkbox(workbox, options) {
+  if (options.config) {
+    // Set workbox config
+    workbox.setConfig(options.config)
+  }
 
-workbox.precaching.cleanupOutdatedCaches()
+  if (options.cacheNames) {
+    // Set workbox cache names
+    workbox.core.setCacheNameDetails(options.cacheNames)
+  }
 
-// --------------------------------------------------
-// Precaches
-// --------------------------------------------------
+  if (options.clientsClaim) {
+    // Start controlling any existing clients as soon as it activates
+    workbox.core.clientsClaim()
+  }
 
-// Precache assets
+  if (options.skipWaiting) {
+    workbox.core.skipWaiting()
+  }
 
-// --------------------------------------------------
-// Runtime Caching
-// --------------------------------------------------
+  if (options.cleanupOutdatedCaches) {
+    workbox.precaching.cleanupOutdatedCaches()
+  }
 
-// Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('https://aghiljose.com/.*'), new workbox.strategies.CacheFirst ({"cacheName":"aghil_jose_050322","cacheExpiration":{"maxEntries":100,"maxAgeSeconds":604800}}), 'GET')
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.StaleWhileRevalidate ({}), 'GET')
-workbox.routing.setCatchHandler(({ url, event, params }) => {
-  const strategy = new workbox.strategies.NetworkFirst({
-    networkTimeoutSeconds: 10
-  });
-  return strategy.handle({
-    request: new Request(url)
-  });
-});
+  if (options.offlineAnalytics) {
+    // Enable offline Google Analytics tracking
+    workbox.googleAnalytics.initialize()
+  }
+}
+
+function precacheAssets(workbox, options) {
+  if (options.preCaching.length) {
+    workbox.precaching.precacheAndRoute(options.preCaching, options.cacheOptions)
+  }
+}
+
+
+function runtimeCaching(workbox, options) {
+  const requestInterceptor = {
+    requestWillFetch({ request }) {
+      if (request.cache === 'only-if-cached' && request.mode === 'no-cors') {
+        return new Request(request.url, { ...request, cache: 'default', mode: 'no-cors' })
+      }
+      return request
+    },
+    fetchDidFail(ctx) {
+      ctx.error.message =
+        '[workbox] Network request for ' + ctx.request.url + ' threw an error: ' + ctx.error.message
+      console.error(ctx.error, 'Details:', ctx)
+    },
+    handlerDidError(ctx) {
+      ctx.error.message =
+        `[workbox] Network handler threw an error: ` + ctx.error.message
+      console.error(ctx.error, 'Details:', ctx)
+      return null
+    }
+  }
+
+  for (const entry of options.runtimeCaching) {
+    const urlPattern = new RegExp(entry.urlPattern)
+    const method = entry.method || 'GET'
+
+    const plugins = (entry.strategyPlugins || [])
+      .map(p => new (getProp(workbox, p.use))(...p.config))
+
+    plugins.unshift(requestInterceptor)
+
+    const strategyOptions = { ...entry.strategyOptions, plugins }
+
+    const strategy = new workbox.strategies[entry.handler](strategyOptions)
+
+    workbox.routing.registerRoute(urlPattern, strategy, method)
+  }
+}
+
+function offlinePage(workbox, options) {
+  if (options.offlinePage) {
+    // Register router handler for offlinePage
+    workbox.routing.registerRoute(new RegExp(options.pagesURLPattern), ({ request, event }) => {
+      const strategy = new workbox.strategies[options.offlineStrategy]
+      return strategy
+        .handle({ request, event })
+        .catch(() => caches.match(options.offlinePage))
+    })
+  }
+}
+
+function workboxExtensions(workbox, options) {
+  
+}
+
+function cachingExtensions(workbox, options) {
+  
+}
+
+function routingExtensions(workbox, options) {
+  
+}
